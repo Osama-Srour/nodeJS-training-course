b2d51057e7d44d58680b3920cf213ce054010453
Osama Srour <osamasrour0@gmail.com>
2023-08-23 12:19:39 +0400

    third-problem-solving-question

--- third-problem-solving-question/method-1.js
+++ third-problem-solving-question/method-1.js
@@ -1,5 +1,10 @@
+console.log(ReverseString("Hello, World!")); //!dlroW ,olleH
+console.log(ReverseString("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));//ZYXWVUTSRQPONMLKJIHGFEDCBA
+console.log(ReverseString("Can you do it , for this , text !?")); //?! txet , siht rof , ti od uoy naC
+console.log(ReverseString("?! txet , siht rof , ti od uoy naC")); // Can you do it , for this , text !?
+
+
+//convert to array then reverse  and join it by array methods; easy way 
 function ReverseString(str){
    return str.split('').reverse().join('');
 }
-
-console.log(ReverseString("Hello, World!"));
--- third-problem-solving-question/method-2.js
+++ third-problem-solving-question/method-2.js
@@ -1,14 +1,18 @@
+
+console.log(ReverseString("Hello, World!")); //!dlroW ,olleH
+console.log(ReverseString("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));//ZYXWVUTSRQPONMLKJIHGFEDCBA
+console.log(ReverseString("Can you do it , for this , text !?")); //?! txet , siht rof , ti od uoy naC
+console.log(ReverseString("?! txet , siht rof , ti od uoy naC")); // Can you do it , for this , text !?
+
+//loop the str from last to first and save elemnet in results then return it ;  
 function ReverseString(str){
 
-    let reverse = '';
-   
+    let results  = '';   
     for( i =str.length -1 ; i >= 0 ; i--){
-            reverse += str[i];
-   
+            results  += str[i];
         }
-    return reverse;
+    return results ;
     
 }
 
 
-console.log(ReverseString("Hello, World!"));
