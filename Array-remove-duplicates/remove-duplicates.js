8a16a80bbc36b178dd145990cc273a3ef875f9dc
Osama Srour <osamasrour0@gmail.com>
2023-08-27 14:32:06 +0400

    remove-duplicate

---
+++ Array-remove-duplicates/remove-duplicates.js
@@ -0,0 +1,31 @@
+
+const arrOfNum = [1, 2, 2, 4, 5, 6, 6];
+
+const getUniqueValues = (arrOfNum) => {
+     
+    let uniqueArray = [];
+
+        //loop for all item in oregenal array 
+     for( num of arrOfNum){
+
+         //check if item avalable in new array 
+        let isNumberExist = false ;  
+        for(unique of uniqueArray){
+
+            if(num === unique){
+                isNumberExist = true;
+                console.log('num === unique');
+                break ; 
+            }
+        }
+
+        //add item to new array if item does not exist 
+        if(!isNumberExist){
+            uniqueArray.push(num);
+        }    
+     }
+
+     //return array 
+      return uniqueArray
+};
+console.log(getUniqueValues(arrOfNum)); // [1, 2, 4, 5, 6] 
