768c7e844d385be5a7cd64db716ca491d271370c
Osama Srour <osamasrour0@gmail.com>
2023-09-12 15:49:45 +0400

    PC Information

--- Homework/find-my-device-details/index.js
+++ Homework/find-my-device-details/index.js
@@ -4,37 +4,48 @@ const cors = require('cors')
 const app = express()
 const apiPort = 3000
 const os = require('os');
+const fs = require('fs');
+const { off } = require('process')
 
 
 
 app.set('views', './view');
 app.set('view engine', 'ejs');
+
+const info = {
+    hostname: os.hostname(),
+    operation_system: os.type(),
+    arch: os.arch(),
+    cpu: os.cpus()[0]["model"],
+    ram: `${formatBytes(os.freemem())} free of : ${formatBytes(os.totalmem())}`,
+};
+
+
 app.get('/', (req, res) => {
-    const info = {
-        hostname: os.hostname(),
-        operation_system: os.type(),
-        arch: os.arch(),
-        cpu: os.cpus()[0]["model"],
-        ram: `${formatBytes(os.freemem())} free of : ${formatBytes(os.totalmem())}`,
-    } ; 
-    res.render('list.ejs', {info});
     
-    ////console.log(os.arch()); // 64 , 32
-    ////console.log(os.type()); // operating system
-    ////console.log(os.hostname()); // pc name 
-    ////console.log(os.cpus()); // CPU info 
-    ////console.log(formatBytes(os.totalmem())); // Total  space 
-    ////console.log(formatBytes(os.freemem()));// free space 
-    // return res.json({
-    //     hostname: os.hostname() , 
-    //     operation_system: os.type() , 
-    //     arch: os.arch(),
-    //     cpu: os.cpus()[0]["model"],
-    //     ram: `${formatBytes(os.freemem())} free of : ${formatBytes(os.totalmem())}`   , 
-    // })
+    res.render('list.ejs', {info});
+})
+
+app.get('/Exportdata', (req, res) => {
+    
+    const str = `Hostname: ${info.hostname.trim() }
+    operation_system : ${info.operation_system.trim() } 
+    cpu : ${info.cpu.trim()}
+    ram: ${ info.ram }`
+
+    fs.writeFile('PC Information.txt', str, (err) => {
+        if (err) {
+            res.status(400).json( {"Error" : err} );
+
+        } else {
+            res.status(200).send(" \"PC Information.txt\" has been created in your device!" );
+        }
 
 
+    })
 
+
+        
 })
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
@@ -53,3 +64,6 @@ function formatBytes(bytes) {
 
     return size.toFixed(2) + ' ' + units[unitIndex];
 }
+
+
+
